d051a1d2815d4da778de078ca2f5aa513e4e488e
HermanoCastro65 <hermanocastro18@gmail.com>
2023-01-27 15:41:13 -0300

    create an instance of userRepository and use it in method listUsers

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { UserRepository } from './user.repository'
 
 @Controller('/users')
 export class UserController {
+  private userRepository = new UserRepository()
+
   @Post()
   async createUser(@Body() userData) {
+    this.userRepository.save(userData)
     return userData
+  }
+
+  @Get()
+  async listUsers() {
+    this.userRepository.list()
   }
 }
